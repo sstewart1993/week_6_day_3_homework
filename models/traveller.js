2114243cf15d65dfb30500b73c6dd85843aac782
CodeClan Student <codeclanstudent@codeclans-mbp-5.home>
2020-10-07 17:43:30 +0100

    half way

--- models/traveller.js
+++ models/traveller.js
@@ -9,24 +9,35 @@ Traveller.prototype.getJourneyStartLocations = function() {
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation
+  })
 
 };
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport
+  })
 
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance
+  })
+ 
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.map((total, journey) => {
+    return total += journey.distance
+  })
 };
 
-Traveller.prototype.getUniqueModesOfTransport = function () {
+// Traveller.prototype.getUniqueModesOfTransport = function () {
 
-};
+// };
 
 
 module.exports = Traveller;
